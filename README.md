a6c8e8bebcd63557117d3ca9ba1f30703669cb1d
Okafor Nelson <okafornel@gmail.com>
2022-02-16 09:53:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+Library through the console
+This is a program that accepts input from a user to add information about a book, and displays them on the console.
+
+Table of contents
+Overview
+The challenge
+Links
+My process
+Built with
+What I learned
+Continued development
+Useful resources
+Author
+Note: Delete this note and update the table of contents based on what sections you keep.
+
+Overview
+The challenge
+Users should be able to:
+
+Add book name, author name and rating for the book.
+See books displayed in the console after adding them.
+
+
+Links
+Solution URL: Add solution URL here
+Live Site URL: Add live site URL here
+My process
+Built with
+Python basics
+
+What I learned
+I learnt how to properly use functions, to make sure a function performs just one job.
+Learnt how to manipulate data in a dictionary.
+
+To see how you can add code snippets, see below:
+
+<h1>Some codes I'm proud of</h1>
+def valid_input(prompt, values):
+        choice = input(prompt)
+        if choice in values:
+            return choice
+        else:
+            print_pause('Please put in a valid choice')
+
+def add_books(name, author, rating):
+        book = {"name_of_book": name,
+                "name_of_author": author,
+                "book_rating": rating}
+        our_library.append(book)
+        add_more_books()
+
+Continued development
+I'm currently learning Python for Data Science and wanted to use knowledge from what I've learnt to practice. I hope to move further into learning MySQL and other programs (Pandas)
+
+Useful resources
+Ustacky - This is my main resource for learning Data Science with Python.
+Udacity - My initiate introduction to Python was from this resource. It opened me up to accepting and loving Python.
+
+Author
+Website - Add your name here
+Twitter - @NELtheDev
---
+++ main.py
@@ -0,0 +1,67 @@
+import time
+
+
+# Make a book list that asks for input and pushes
+# it to a list which displays a list of books
+def library():
+    our_library = []
+
+    def print_pause(message):
+        time.sleep(1)
+        print(message)
+
+    def valid_input(prompt, values):
+        choice = input(prompt)
+        if choice in values:
+            return choice
+        else:
+            print_pause('Please put in a valid choice')
+
+    def valid_input_number(prompt):
+        choice = input(prompt)
+        choice = float(choice)
+        if type(choice) is float:
+            return choice
+        else:
+            print_pause('Please input correct data')
+            valid_input_number(prompt)
+
+    def user_input():
+        book_name = input("What is the name of the book?: ").title()
+        author = input("What is name of author?: ").title()
+        rating = valid_input_number("Rating of book out of 10?: ")
+
+        while rating > 10:
+            print_pause('Please select a figure greater than 0 and less than 10')
+            rating = valid_input_number("Rating of book out of 10?: ")
+
+        add_books(book_name, author, rating)
+
+    def add_books(name, author, rating):
+        book = {"name_of_book": name,
+                "name_of_author": author,
+                "book_rating": rating}
+        our_library.append(book)
+        add_more_books()
+
+    def print_books():
+        for book in our_library:
+            for key, value in book.items():
+                print(key + ":", value)
+                print("\n")
+
+    def add_more_books():
+        user_choice = valid_input("Do you want to add more books? "
+                                  "choose an option[y/n]: ", ['y', 'n'])
+        if user_choice == 'y':
+            user_input()
+        elif user_choice == 'n':
+            print_pause('Thank you for using our Library')
+            print_books()
+        else:
+            add_more_books()
+
+    add_more_books()
+
+
+library()
